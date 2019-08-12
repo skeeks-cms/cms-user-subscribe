102a6048716e19cc32c79b72f5b6bdfcacfe7fca
semenov <semenov@skeeks.com>
2019-08-12 23:36:04 +0300

    dev

---
+++ src/config/web.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 15.06.2015
+ */
+return [
+    'modules' =>
+    [
+        'usersubscribe' => [
+            'class'         => '\skeeks\cms\usersubscribe\ModuleUserSubscribe',
+        ]
+    ]
+];
